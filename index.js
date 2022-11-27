6d6058150cd79847b1f96ed55273c61716c26785
mdrobiulislam09 <robiul.rubin@gmail.com>
2022-11-28 01:35:06 +0600

    vercel file added

--- index.js
+++ index.js
@@ -13,11 +13,6 @@ app.get('/', async (req, res) => {
     res.send('server is running on ok')
 })
 
-
-
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.zlkgiha.mongodb.net/?retryWrites=true&w=majority`;
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.jm7v6w9.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
